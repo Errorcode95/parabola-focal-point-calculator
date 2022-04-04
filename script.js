d966bfd22d4d4af5be671f2852c2867fb4d31ace
Flare <jetcadwallader@gmail.com>
2022-04-04 23:07:11 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var parabolaWidth = 150
+var parabolaDepth = 25
+var output
+
+var X = parabolaWidth / 2
+var X2 = X * X
+
+var A4 = parabolaDepth * 4
+
+output = X2 / A4
+
+console.log ("Parabola Width: "+parabolaWidth)
+console.log ("Parabola Depth: "+parabolaDepth)
+console.log ("X: "+X)
+console.log ("X2: "+X2)
+console.log ("A4: "+A4)
+console.log ("Output: "+output)
