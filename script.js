17f93b7736f1f74483d57f9df5da5f82915c0653
Flare <jetcadwallader@gmail.com>
2022-04-12 11:58:59 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//IO
+var parabolaWidth = 150
+var parabolaDepth = 25
+var output
+
+//focal point calculator
+var X = parabolaWidth / 2
+var X2 = X * X
+
+var A4 = parabolaDepth * 4
+
+output = X2 / A4
+document.getElementById("output").innerHTML = output;
+console.log (output);
+
+/*
+//scaling calculator
+var xScale =  560 / parabolaWidth
+
+
+var c = document.getElementById("preview");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(20, 300);
+ctx.quadraticCurveTo(300, parabolaDepth * xScale + 300, 580, 300);
+ctx.stroke();
+
+ctx.fillRect(300, output * xScale,1,1);
+
+ctx.beginPath();
+ctx.moveTo(300, parabolaDepth * xScale + 300);
+ctx.lineTo(300, output * xScale);
+ctx.stroke(); 
+*/
